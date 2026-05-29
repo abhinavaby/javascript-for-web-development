5336d943144bde9689f3cbaa2051f61d58d9304d
abhinavaby <abhinavaby07@gmail.com>
2026-05-29 10:54:25 +0530

    variables

--- chapter 1/first_js_code.js
+++ chapter 1/first_js_code.js
@@ -1,1 +1,5 @@
+//n JavaScript, console.log() is a built-in function used to print text, variables, or objects to the web console. It is primarily used by developers to debug code, test logic, and inspect data values in real time
+
+
 console.log("hello world");
+console.log("this is abhinav")
---
+++ chapter 1/variables.js
@@ -0,0 +1,13 @@
+// in JavaScript, a variable is a named container used to store data values that can be referenced and manipulated in your code.
+name="abhinav"
+age=22;
+price=3.22;
+console.log(`hi ${name} you are ${age} years old`);
+console.log(price);
+x=null;
+y=undefined;
+console.log(`x=${x} y=${y}`);
+ifFalse=true;
+console.log(ifFalse);
+name=22;
+console.log(name);
