1e4a4079ff0892382ca473f96c91d30fca7903b0
abhinavaby <abhinavaby07@gmail.com>
2026-05-30 10:52:57 +0530

    practice 2

---
+++ chapter 2/practice2.js
@@ -0,0 +1,16 @@
+let mark=prompt("enter the mark: ");
+if(mark>=90){
+    alert("Grade: A");
+}
+else if(mark>=70){
+    alert("Grade: B");
+}
+else if(mark>=60){
+    alert("Grade: C");
+}
+else if(mark>=50){
+    alert("Grade: d");
+}
+else{
+    alert("failed")
+}
