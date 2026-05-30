8967f99de49782be2ba07e42b061879745fbd650
abhinavaby <abhinavaby07@gmail.com>
2026-05-30 12:22:49 +0530

    add

---
+++ chapter 3/practice2.js
@@ -0,0 +1,9 @@
+// number guessing game
+let b=23;
+let number=prompt("enter the number: ");
+while(b!=number){
+    
+    number=prompt("wrong guess , enter the number: ");
+
+}
+alert("congrats correct guess");
