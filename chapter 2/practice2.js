a699d292ec9d0abfc694a1577c20784cdd6dcb82
abhinavaby <abhinavaby07@gmail.com>
2026-05-30 11:40:49 +0530

    changed chapter 2

--- chapter 2/practice2.js
+++ chapter 2/practice2.js
@@ -1,5 +1,5 @@
 let mark=prompt("enter the mark: ");
-if(mark>=90){
+if(mark>=90 && mark<=100){
     alert("Grade: A");
 }
 else if(mark>=70){
@@ -9,7 +9,7 @@ else if(mark>=60){
     alert("Grade: C");
 }
 else if(mark>=50){
-    alert("Grade: d");
+    alert("Grade: D ");
 }
 else{
     alert("failed")
